6e2c98977496ab92151c004fa11a1a9ddefbd3a5
EkanayakeDMaheesha <135611931+EkanayakeDMaheesha@users.noreply.github.com>
2023-07-26 23:38:06 +0530

    Add files via upload

---
+++ tutorial7.5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void Q5()
+{
+    int n1,n2;
+    printf("Énter two numbers: ");
+    scanf("%d %d",&n1,&n2);
+
+    printf("\nSum: %d\n\n",(n1+n2));
+}
+int main()
+{
+    Q5();
+    Q5();
+    Q5();
+    Q5();
+    Q5();
+
+}
